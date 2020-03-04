3c76dcd89d7e428ff0c582f7dcc80d239d67b1d3
flexih <flexih@gmail.com>
2020-03-04 21:16:05 +0800

    storyboard supports IBOutlet

--- storyboard/storyboard.py
+++ storyboard/storyboard.py
@@ -9,14 +9,29 @@ class Vistor(xml.sax.ContentHandler):
     def __init__(self):
         self.tags = {}
         self.actions = []
+        self.propertys = []
+        self.outlets = []
+        self.customClass = ''
     def startElement(self, tag, attributes):
         if tag == 'viewController':
-            self.tags[attributes['id']] = attributes['customClass']
+            self.customClass = attributes['customClass']
+            self.tags[attributes['id']] = self.customClass
         elif tag == 'action' and attributes.has_key('selector'):
             self.actions.append((attributes['selector'], attributes['destination']))
+        elif tag == 'outlet' and attributes.has_key('property'):
+            self.propertys.append(attributes['property'])
+    def endElement(self, tag):
+        if tag == 'viewController':
+            self.outlets.append((self.customClass, self.propertys))
+            self.propertys = []
+            self.customClass = ''
     def endDocument(self):
         for action in self.actions:
             print '-[{0} {1}]'.format(self.tags[action[1]], action[0])
+        for outlet in self.outlets:
+            for prop in outlet[1]:
+                print '-[{0} get{1}:]'.format(outlet[0], prop.capitalize())
+                print '-[{0} set{1}:]'.format(outlet[0], prop.capitalize())
 
 def handleStoryboard(path):
     parser = xml.sax.make_parser()
